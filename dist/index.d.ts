6203a9e24234f5aa6df4434a0618199fb5717b02
Hai Phan <hai.phan@gmail.com>
2023-01-07 14:11:41 -0800

    move Janus base type declarations to janus.d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
+import { JanusMediaOptions, JanusMessage, JanusMid, JanusPluginHandle, JanusPublishOptions, JanusSessionOptions, JanusStreamSpec, JanusSubscriberConfigureOptions, JanusWatchOptions, Jsep } from "./janus";
 interface VideoRoomClient {
     createSession(server: string | string[], options?: JanusSessionOptions): Promise<VideoRoomSession>;
 }
@@ -61,12 +62,7 @@ interface StreamingSubscriber {
     }): Promise<void>;
     unsubscribe(): Promise<void>;
 }
-interface JanusPluginHandleEx {
-    createOffer(options: unknown): void;
-    createAnswer(options: unknown): void;
-    handleRemoteJsep(options: unknown): void;
-    send(options: unknown): void;
-    detach(options: unknown): void;
+interface JanusPluginHandleEx extends JanusPluginHandle {
     eventTarget: ReturnType<typeof makeEventTarget>;
     sendRequest(message: JanusMessage): Promise<JanusMessage>;
     sendAsyncRequest(options: {
@@ -78,79 +74,6 @@ interface JanusPluginHandleEx {
 interface AsyncResponse {
     message: JanusMessage;
     jsep?: Jsep;
-}
-type JanusMessage = {
-    [key: string]: any;
-};
-interface Jsep {
-    sdp: string;
-}
-interface JanusStreamSpec {
-    feed: unknown;
-    mid?: JanusMid;
-}
-type JanusMid = unknown;
-interface JanusSessionOptions {
-    iceServers?: string[];
-    ipv6?: boolean;
-    withCredentials?: boolean;
-    max_poll_events?: number;
-    destroyOnUnload?: boolean;
-    token?: unknown;
-    apisecret?: string;
-}
-interface JanusPublishOptions {
-    audiocodec?: string;
-    videocodec?: string;
-    bitrate?: number;
-    record?: boolean;
-    filename?: string;
-    display?: string;
-    audio_level_average?: number;
-    audio_active_packets?: number;
-    descriptions?: {
-        mid: JanusMid;
-        description: string;
-    }[];
-}
-interface JanusWatchOptions {
-    pin?: string;
-    media?: string[];
-}
-interface JanusMediaOptions {
-    tracks?: JanusTrackSpec[];
-    trickle?: boolean;
-    stream?: MediaStream;
-    customizeSdp?: (jsep: Jsep) => void;
-    customizeRemoteSdp?: (jsep: Jsep) => void;
-}
-interface JanusTrackSpec {
-    type: string;
-    mid?: JanusMid;
-    capture?: boolean | string | {
-        deviceId: unknown;
-        width?: number;
-        height?: number;
-    };
-    simulcast?: boolean;
-    svc?: unknown;
-    recv?: boolean;
-    add?: boolean;
-    replace?: boolean;
-    remove?: boolean;
-    dontStop?: boolean;
-    transforms?: unknown;
-}
-interface JanusSubscriberConfigureOptions {
-    mid?: JanusMid;
-    send?: boolean;
-    substream?: number;
-    temporal?: number;
-    fallback?: number;
-    spatial_layer?: number;
-    temporal_layer?: number;
-    audio_level_average?: number;
-    audio_active_packets?: number;
 }
 export declare function createVideoRoomClient(options?: {
     debug?: boolean | string[];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { JanusSession } from "./janus"
+import { JanusMediaOptions, JanusMessage, JanusMid, JanusPluginHandle, JanusPublishOptions, JanusSession, JanusSessionOptions, JanusStreamSpec, JanusSubscriberConfigureOptions, JanusWatchOptions, Jsep } from "./janus"
 
 interface VideoRoomClient {
     createSession(server: string|string[], options?: JanusSessionOptions): Promise<VideoRoomSession>
@@ -56,13 +56,7 @@ interface StreamingSubscriber {
     unsubscribe(): Promise<void>
 }
 
-interface JanusPluginHandleEx {
-    createOffer(options: unknown): void
-    createAnswer(options: unknown): void
-    handleRemoteJsep(options: unknown): void
-    send(options: unknown): void
-    detach(options: unknown): void
-
+interface JanusPluginHandleEx extends JanusPluginHandle {
     eventTarget: ReturnType<typeof makeEventTarget>
     sendRequest(message: JanusMessage): Promise<JanusMessage>
     sendAsyncRequest(options: {message: JanusMessage, jsep?: Jsep, expectResponse: (response: AsyncResponse) => boolean}): Promise<AsyncResponse>
@@ -71,80 +65,6 @@ interface JanusPluginHandleEx {
 interface AsyncResponse {
     message: JanusMessage
     jsep?: Jsep
-}
-
-type JanusMessage = {[key: string]: any}
-
-interface Jsep {
-    sdp: string
-}
-
-interface JanusStreamSpec {
-    feed: unknown
-    mid?: JanusMid
-}
-
-type JanusMid = unknown
-
-interface JanusSessionOptions {
-    iceServers?: string[]
-    ipv6?: boolean
-    withCredentials?: boolean
-    max_poll_events?: number
-    destroyOnUnload?: boolean
-    token?: unknown
-    apisecret?: string
-}
-
-interface JanusPublishOptions {
-    audiocodec?: string
-    videocodec?: string
-    bitrate?: number
-    record?: boolean
-    filename?: string
-    display?: string
-    audio_level_average?: number
-    audio_active_packets?: number
-    descriptions?: {mid: JanusMid, description: string}[]
-}
-
-interface JanusWatchOptions {
-    pin?: string
-    media?: string[]
-}
-
-interface JanusMediaOptions {
-    tracks?: JanusTrackSpec[]
-    trickle?: boolean
-    stream?: MediaStream
-    customizeSdp?: (jsep: Jsep) => void
-    customizeRemoteSdp?: (jsep: Jsep) => void
-}
-
-interface JanusTrackSpec {
-    type: string
-    mid?: JanusMid
-    capture?: boolean|string|{deviceId: unknown, width?: number, height?: number}
-    simulcast?: boolean
-    svc?: unknown
-    recv?: boolean
-    add?: boolean
-    replace?: boolean
-    remove?: boolean
-    dontStop?: boolean
-    transforms?: unknown
-}
-
-interface JanusSubscriberConfigureOptions {
-    mid?: JanusMid
-    send?: boolean
-    substream?: number
-    temporal?: number
-    fallback?: number
-    spatial_layer?: number
-    temporal_layer?: number
-    audio_level_average?: number
-    audio_active_packets?: number
 }
 
 
